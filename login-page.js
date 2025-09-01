dc6a17ef06565629362940523712b11ea100a835
AslanSoft <beggyplayy@gmail.com>
2025-09-01 20:45:37 +0500

    ss

--- login-page.js
+++ login-page.js
@@ -2,19 +2,20 @@ window.addEventListener("DOMContentLoaded", () => {
   const button = document.getElementById("loginBtn");
 
   document.getElementById("loginBtn").addEventListener("click", function(e) {
-      e.preventDefault();
+    e.preventDefault();
 
-      const username = document.getElementById("username").value.trim();
-      const password = document.getElementById("password").value.trim();
-      const error = document.getElementById("error");
+    const username = document.getElementById("username").value.trim();
+    const password = document.getElementById("password").value.trim();
+    const error = document.getElementById("error");
 
-      const correctUsername = "aslan";
-      const correctPassword = "1720204";
+    const correctUsername = "aslan";
+    const correctPassword = "1720204";
 
-      if (username === correctUsername && password === correctPassword) {
-        // Перенаправляем на домашнюю страницу
-        window.location.href = "./home.html"; 
-      } else {
-        error.style.display = "block";
-      }
-    });
+    if (username === correctUsername && password === correctPassword) {
+      // Перенаправляем на домашнюю страницу
+      window.location.href = "./home.html"; 
+    } else {
+      error.style.display = "block";
+    }
+  });
+});
